95bb774ee90e243fdf449da838a214c3ff90944c
Matt Sherman <mwsherman@gmail.com>
2020-04-25 18:37:24 -0400

    Inline the WB rules; benchmarks ~10% faster

--- words/scanner.go
+++ words/scanner.go
@@ -78,47 +78,203 @@ func (sc *Scanner) Scan() bool {
 			sc.buffer = append(sc.buffer, r)
 		}
 
-		switch {
-		case sc.wb1():
-			// true indicates continue
+		// Rules: https://unicode.org/reports/tr29/#Word_Boundary_Rules
+
+		sot := sc.pos == 0 // "start of text"
+		eof := len(sc.buffer) == sc.pos
+
+		// WB1
+		if sot && !eof {
 			sc.accept()
 			continue
-		case sc.wb2():
-			// true indicates break
+		}
+
+		// WB2
+		if eof {
 			break
-		case
-			sc.wb3():
-			// true indicates continue
+		}
+
+		current := sc.buffer[sc.pos]
+		previous := sc.buffer[sc.pos-1]
+
+		// WB3
+		if is(LF, current) && is(CR, previous) {
 			sc.accept()
 			continue
-		case
-			sc.wb3a(),
-			sc.wb3b():
-			// true indicates break
+		}
+
+		// WB3a
+		if is(_mergedCRLFNewline, previous) {
 			break
-		case
-			sc.wb3c(),
-			sc.wb3d(),
-			sc.wb4(),
-			sc.wb5(),
-			sc.wb6(),
-			sc.wb7(),
-			sc.wb7a(),
-			sc.wb7b(),
-			sc.wb7c(),
-			sc.wb8(),
-			sc.wb9(),
-			sc.wb10(),
-			sc.wb11(),
-			sc.wb12(),
-			sc.wb13(),
-			sc.wb13a(),
-			sc.wb13b(),
-			sc.wb15(),
-			sc.wb16():
-			// true indicates continue
+		}
+
+		// WB3b
+		if is(_mergedCRLFNewline, current) {
+			break
+		}
+
+		// WB3c
+		if is(emoji.Extended_Pictographic, current) && is(ZWJ, previous) {
 			sc.accept()
 			continue
+		}
+
+		// WB3d
+		if is(WSegSpace, current) && is(WSegSpace, previous) {
+			sc.accept()
+			continue
+		}
+
+		// WB4
+		if is(_mergedExtendFormatZWJ, current) {
+			sc.accept()
+			continue
+		}
+
+		// WB5
+		if is(AHLetter, current) && sc.seekPrevious(sc.pos, AHLetter) {
+			sc.accept()
+			continue
+		}
+
+		// WB6
+		if is(_mergedMidLetterMidNumLetQ, current) && sc.seekForward(AHLetter) && sc.seekPrevious(sc.pos, AHLetter) {
+			sc.accept()
+			continue
+		}
+
+		// WB7
+		if is(AHLetter, current) {
+			previousIndex := sc.seekPreviousIndex(sc.pos, _mergedMidLetterMidNumLetQ)
+			if previousIndex >= 0 && sc.seekPrevious(previousIndex, AHLetter) {
+				sc.accept()
+				continue
+			}
+		}
+
+		// WB7a
+		if is(Single_Quote, current) && sc.seekPrevious(sc.pos, Hebrew_Letter) {
+			sc.accept()
+			continue
+		}
+
+		// WB7b
+		if is(Double_Quote, current) && sc.seekForward(Hebrew_Letter) && sc.seekPrevious(sc.pos, Hebrew_Letter) {
+			sc.accept()
+			continue
+		}
+
+		// WB7c
+		if is(Hebrew_Letter, current) {
+			previousIndex := sc.seekPreviousIndex(sc.pos, Double_Quote)
+			if previousIndex >= 0 && sc.seekPrevious(previousIndex, Hebrew_Letter) {
+				sc.accept()
+				continue
+			}
+		}
+
+		// WB8
+		if is(Numeric, current) && sc.seekPrevious(sc.pos, Numeric) {
+			sc.accept()
+			continue
+		}
+
+		// WB9
+		if is(Numeric, current) && sc.seekPrevious(sc.pos, AHLetter) {
+			sc.accept()
+			continue
+		}
+
+		// WB10
+		if is(AHLetter, current) && sc.seekPrevious(sc.pos, Numeric) {
+			sc.accept()
+			continue
+		}
+
+		// WB11
+		if is(Numeric, current) {
+			previousIndex := sc.seekPreviousIndex(sc.pos, _mergedMidNumMidNumLetQ)
+			if previousIndex >= 0 && sc.seekPrevious(previousIndex, Numeric) {
+				sc.accept()
+				continue
+			}
+		}
+
+		// WB12
+		if is(_mergedMidNumMidNumLetQ, current) && sc.seekForward(Numeric) && sc.seekPrevious(sc.pos, Numeric) {
+			sc.accept()
+			continue
+		}
+
+		// WB13
+		if is(Katakana, current) && sc.seekPrevious(sc.pos, Katakana) {
+			sc.accept()
+			continue
+		}
+
+		// WB13a
+		if is(ExtendNumLet, current) && sc.seekPrevious(sc.pos, _mergedAHLetterNumericKatakanaExtendNumLet) {
+			sc.accept()
+			continue
+		}
+
+		// WB13b
+		if is(_mergedAHLetterNumericKatakana, current) && sc.seekPrevious(sc.pos, ExtendNumLet) {
+			sc.accept()
+			continue
+		}
+
+		// WB15
+		if is(Regional_Indicator, current) {
+			ok := true
+
+			// Buffer comprised entirely of an odd number of RI, ignoring Extend|Format|ZWJ
+			count := 0
+			for i := sc.pos - 1; i >= 0; i-- {
+				r := sc.buffer[i]
+				if is(_mergedExtendFormatZWJ, r) {
+					continue
+				}
+				if !is(Regional_Indicator, r) {
+					ok = false
+					break
+				}
+				count++
+			}
+
+			// If we fall through, we've seen the whole buffer,
+			// so it's all Regional_Indicator | Extend|Format|ZWJ
+			if ok {
+				odd := count > 0 && count%2 == 1
+				if odd {
+					sc.accept()
+					continue
+				}
+			}
+		}
+
+		// WB16
+		if is(Regional_Indicator, current) {
+			ok := false
+			// Last n runes represent an odd number of RI, ignoring Extend|Format|ZWJ
+			count := 0
+			for i := sc.pos - 1; i >= 0; i-- {
+				r := sc.buffer[i]
+				if is(_mergedExtendFormatZWJ, r) {
+					continue
+				}
+				if !is(Regional_Indicator, r) {
+					odd := count > 0 && count%2 == 1
+					ok = odd
+					break
+				}
+				count++
+			}
+
+			if ok {
+				sc.accept()
+				continue
+			}
 		}
 
 		// If we fall through all the above rules, it's a word break, aka WB999
@@ -147,70 +303,6 @@ func (sc *Scanner) Err() error {
 // In most cases, returning true means 'keep going'; check the name of the return var for clarity
 
 var is = unicode.Is
-
-// wb1 implements https://unicode.org/reports/tr29/#WB1
-func (sc *Scanner) wb1() (accept bool) {
-	sot := sc.pos == 0 // "start of text"
-	eof := len(sc.buffer) == sc.pos
-	return sot && !eof
-}
-
-// wb2 implements https://unicode.org/reports/tr29/#WB2
-func (sc *Scanner) wb2() (breaking bool) {
-	// eof
-	return len(sc.buffer) == sc.pos
-}
-
-// wb3 implements https://unicode.org/reports/tr29/#WB3
-func (sc *Scanner) wb3() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(LF, current) {
-		return false
-	}
-
-	previous := sc.buffer[sc.pos-1]
-	return is(CR, previous)
-}
-
-// wb3a implements https://unicode.org/reports/tr29/#WB3a
-func (sc *Scanner) wb3a() (breaking bool) {
-	previous := sc.buffer[sc.pos-1]
-	return is(_mergedCRLFNewline, previous)
-}
-
-// wb3b implements https://unicode.org/reports/tr29/#WB3b
-func (sc *Scanner) wb3b() (breaking bool) {
-	current := sc.buffer[sc.pos]
-	return is(_mergedCRLFNewline, current)
-}
-
-// wb3c implements https://unicode.org/reports/tr29/#WB3c
-func (sc *Scanner) wb3c() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(emoji.Extended_Pictographic, current) {
-		return false
-	}
-
-	previous := sc.buffer[sc.pos-1]
-	return is(ZWJ, previous)
-}
-
-// wb3d implements https://unicode.org/reports/tr29/#WB3d
-func (sc *Scanner) wb3d() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(WSegSpace, current) {
-		return false
-	}
-
-	previous := sc.buffer[sc.pos-1]
-	return is(WSegSpace, previous)
-}
-
-// wb4 implements https://unicode.org/reports/tr29/#WB4
-func (sc *Scanner) wb4() (accept bool) {
-	current := sc.buffer[sc.pos]
-	return is(_mergedExtendFormatZWJ, current)
-}
 
 // seekForward looks ahead until it hits a rune satisfying one of the range tables,
 // ignoring Extend|Format|ZWJ
@@ -270,224 +362,6 @@ func (sc *Scanner) seekPreviousIndex(pos int, rts ...*unicode.RangeTable) int {
 // Logic is here: https://unicode.org/reports/tr29/#Grapheme_Cluster_and_Format_Rules (driven by WB4)
 func (sc *Scanner) seekPrevious(pos int, rts ...*unicode.RangeTable) bool {
 	return sc.seekPreviousIndex(pos, rts...) >= 0
-}
-
-// wb5 implements https://unicode.org/reports/tr29/#WB5
-func (sc *Scanner) wb5() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(AHLetter, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, AHLetter)
-}
-
-// wb6 implements https://unicode.org/reports/tr29/#WB6
-func (sc *Scanner) wb6() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !(is(_mergedMidLetterMidNumLetQ, current)) {
-		return false
-	}
-
-	if !sc.seekForward(AHLetter) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, AHLetter)
-}
-
-// wb7 implements https://unicode.org/reports/tr29/#WB7
-func (sc *Scanner) wb7() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !(is(_mergedAHLetterExtendFormatZWJ, current)) {
-		return false
-	}
-
-	previous := sc.seekPreviousIndex(sc.pos, _mergedMidLetterMidNumLetQ)
-	if previous < 0 {
-		return false
-	}
-
-	return sc.seekPrevious(previous, AHLetter)
-}
-
-// wb7a implements https://unicode.org/reports/tr29/#WB7a
-func (sc *Scanner) wb7a() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Single_Quote, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Hebrew_Letter)
-}
-
-// wb7b implements https://unicode.org/reports/tr29/#WB7b
-func (sc *Scanner) wb7b() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Double_Quote, current) {
-		return false
-	}
-
-	if !sc.seekForward(Hebrew_Letter) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Hebrew_Letter)
-}
-
-// wb7c implements https://unicode.org/reports/tr29/#WB7c
-func (sc *Scanner) wb7c() (accept bool) {
-	current := sc.buffer[sc.pos]
-
-	if !is(Hebrew_Letter, current) {
-		return false
-	}
-
-	previous := sc.seekPreviousIndex(sc.pos, Double_Quote)
-	if previous < 0 {
-		return false
-	}
-
-	return sc.seekPrevious(previous, Hebrew_Letter)
-}
-
-// wb8 implements https://unicode.org/reports/tr29/#WB8
-func (sc *Scanner) wb8() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Numeric, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Numeric)
-}
-
-// wb9 implements https://unicode.org/reports/tr29/#WB9
-func (sc *Scanner) wb9() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Numeric, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, AHLetter)
-}
-
-// wb10 implements https://unicode.org/reports/tr29/#WB10
-func (sc *Scanner) wb10() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(AHLetter, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Numeric)
-}
-
-// wb11 implements https://unicode.org/reports/tr29/#WB11
-func (sc *Scanner) wb11() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Numeric, current) {
-		return false
-	}
-
-	previous := sc.seekPreviousIndex(sc.pos, _mergedMidNumMidNumLetQ)
-	if previous < 0 {
-		return false
-	}
-
-	return sc.seekPrevious(previous, Numeric)
-}
-
-// wb12 implements https://unicode.org/reports/tr29/#WB12
-func (sc *Scanner) wb12() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !(is(_mergedMidNumMidNumLetQ, current)) {
-		return false
-	}
-
-	if !sc.seekForward(Numeric) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Numeric)
-}
-
-// wb13 implements https://unicode.org/reports/tr29/#WB13
-func (sc *Scanner) wb13() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Katakana, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, Katakana)
-}
-
-// wb13a implements https://unicode.org/reports/tr29/#WB13a
-func (sc *Scanner) wb13a() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(ExtendNumLet, current) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, _mergedAHLetterNumericKatakanaExtendNumLet)
-}
-
-// wb13b implements https://unicode.org/reports/tr29/#WB13b
-func (sc *Scanner) wb13b() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !(is(_mergedAHLetterNumericKatakana, current)) {
-		return false
-	}
-
-	return sc.seekPrevious(sc.pos, ExtendNumLet)
-}
-
-// wb15 implements https://unicode.org/reports/tr29/#WB15
-func (sc *Scanner) wb15() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Regional_Indicator, current) {
-		return false
-	}
-
-	// Buffer comprised entirely of an odd number of RI, ignoring ExtendFormatZWJ
-	count := 0
-	for i := sc.pos - 1; i >= 0; i-- {
-		r := sc.buffer[i]
-		if is(_mergedExtendFormatZWJ, r) {
-			continue
-		}
-		if !is(Regional_Indicator, r) {
-			return false
-		}
-		count++
-	}
-
-	// If we fall through, we've seen the whole buffer,
-	// so it's all Regional_Indicator | ExtendFormatZWJ
-	odd := count > 0 && count%2 == 1
-	return odd
-}
-
-// wb16 implements https://unicode.org/reports/tr29/#WB16
-func (sc *Scanner) wb16() (accept bool) {
-	current := sc.buffer[sc.pos]
-	if !is(Regional_Indicator, current) {
-		return false
-	}
-
-	// Last n runes represent an odd number of RI, ignoring ExtendFormatZWJ
-	count := 0
-	for i := sc.pos - 1; i >= 0; i-- {
-		r := sc.buffer[i]
-		if is(_mergedExtendFormatZWJ, r) {
-			continue
-		}
-		if !is(Regional_Indicator, r) {
-			odd := count > 0 && count%2 == 1
-			return odd
-		}
-		count++
-	}
-
-	return false
 }
 
 // wb999 implements https://unicode.org/reports/tr29/#WB999
