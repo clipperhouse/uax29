7b26ca4142f707c9019c5f6853b6a03308911d91
Matt Sherman <mwsherman@gmail.com>
2020-04-27 17:07:27 -0400

    Doc update

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
-// Package uax29 provides implementations of Unicode text segmentation: https://unicode.org/reports/tr29/
-// See subpackages for words, sentences and graphemes.
+// Package uax29 provides Unicode text segmentation (UAX #29) for words, sentences and graphemes.
+// See respective sub-packages for those implementations.
+//
+// This top-level package provides base functionality & types.
+//
+// For more information on the UAX #29 spec: https://unicode.org/reports/tr29/
 package uax29
