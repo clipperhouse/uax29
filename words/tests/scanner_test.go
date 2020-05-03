55d4b25825b67141bbaf721124445e14af4ee92a
Matt Sherman <mwsherman@gmail.com>
2020-05-03 14:15:27 -0400

    Update benchmark

--- words/tests/scanner_test.go
+++ words/tests/scanner_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"bufio"
 	"bytes"
 	"io/ioutil"
 	"reflect"
@@ -186,23 +185,18 @@ func BenchmarkScanner(b *testing.B) {
 
 	count := 0
 	for i := 0; i < b.N; i++ {
-		var bb bytes.Buffer
-
 		r := bytes.NewReader(file)
-		sc := bufio.NewScanner(r)
-		sc.Split(words.SplitFunc)
+		sc := words.NewScanner(r)
 
 		c := 0
 		for sc.Scan() {
 			c++
-			bb.WriteString(sc.Text())
 		}
 		if err := sc.Err(); err != nil {
 			b.Error(err)
 		}
 
 		count = c
-		bb.Reset()
 	}
 	b.Logf("%d tokens\n", count)
 }
