5471a10015b60432187407e4ceb27015370362aa
Matt Sherman <mwsherman@gmail.com>
2022-05-25 14:20:57 -0400

    tests

--- iterators/scanner_test.go
+++ iterators/scanner_test.go
@@ -3,7 +3,6 @@ package iterators_test
 import (
 	"bufio"
 	"bytes"
-	"reflect"
 	"testing"
 
 	"github.com/clipperhouse/uax29/iterators"
@@ -15,21 +14,14 @@ func TestScannerSameAsBufio(t *testing.T) {
 
 		r1 := bytes.NewReader(text)
 		sc1 := iterators.NewScanner(r1, bufio.ScanWords)
-		var all1 [][]byte
-		for sc1.Scan() {
-			all1 = append(all1, sc1.Bytes())
-		}
-
 		r2 := bytes.NewReader(text)
 		sc2 := bufio.NewScanner(r2)
 		sc2.Split(bufio.ScanWords)
-		var all2 [][]byte
-		for sc2.Scan() {
-			all2 = append(all2, sc2.Bytes())
-		}
 
-		if !reflect.DeepEqual(all1, all2) {
-			t.Error("iterators.Scanner and bufio.Scanner give different results")
+		for sc1.Scan() && sc2.Scan() {
+			if !bytes.Equal(sc1.Bytes(), sc2.Bytes()) {
+				t.Fatal("Scanner and bufio.Scanner should give identical results")
+			}
 		}
 	}
 }
--- iterators/segmenter_test.go
+++ iterators/segmenter_test.go
@@ -2,11 +2,13 @@ package iterators_test
 
 import (
 	"bufio"
+	"bytes"
 	"math/rand"
 	"reflect"
 	"testing"
 
 	"github.com/clipperhouse/uax29/iterators"
+	"github.com/clipperhouse/uax29/words"
 )
 
 func getRandomBytes() []byte {
@@ -16,42 +18,58 @@ func getRandomBytes() []byte {
 	return b
 }
 
-func TestSegmenter(t *testing.T) {
-	text := []byte("Hello. How are you?")
+func TestSegmenterSameAsScanner(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		text := getRandomBytes()
+		split := words.SplitFunc
 
-	seg := iterators.NewSegmenter(bufio.ScanWords)
-	seg.SetText(text)
+		seg := iterators.NewSegmenter(split)
+		seg.SetText(text)
 
-	for seg.Next() {
-		t.Log(seg.Bytes())
+		r := bytes.NewReader(text)
+		sc := iterators.NewScanner(r, split)
+
+		for seg.Next() && sc.Scan() {
+			if !bytes.Equal(seg.Bytes(), sc.Bytes()) {
+				t.Fatal("Scanner and Segmenter should give identical results")
+			}
+		}
+		if seg.Err() != nil {
+			t.Fatal(seg.Err())
+		}
+		if sc.Err() != nil {
+			t.Fatal(sc.Err())
+		}
 	}
 }
 
-func TestAll(t *testing.T) {
-	text := []byte("Hello. How are you?")
+func TestSegmenterSameAsAll(t *testing.T) {
+	for i := 0; i < 100; i++ {
+		text := getRandomBytes()
 
-	split := bufio.ScanWords
+		split := bufio.ScanWords
 
-	seg := iterators.NewSegmenter(split)
-	seg.SetText(text)
+		seg := iterators.NewSegmenter(split)
+		seg.SetText(text)
 
-	var segResult [][]byte
-	for seg.Next() {
-		segResult = append(segResult, seg.Bytes())
-	}
-	if seg.Err() != nil {
-		t.Fatal(seg.Err())
-	}
+		var segResult [][]byte
+		for seg.Next() {
+			segResult = append(segResult, seg.Bytes())
+		}
+		if seg.Err() != nil {
+			t.Fatal(seg.Err())
+		}
 
-	var allResult [][]byte
-	err := iterators.All(text, &allResult, split)
-	if seg.Err() != nil {
-		t.Fatal(err)
-	}
+		var allResult [][]byte
+		err := iterators.All(text, &allResult, split)
+		if seg.Err() != nil {
+			t.Fatal(err)
+		}
 
-	if !reflect.DeepEqual(segResult, allResult) {
-		t.Logf("Segmenter result: %q", segResult)
-		t.Logf("All result: %q", allResult)
-		t.Fatal("All and Segmenter should give identical results")
+		if !reflect.DeepEqual(segResult, allResult) {
+			t.Logf("Segmenter result: %q", segResult)
+			t.Logf("All result: %q", allResult)
+			t.Fatal("All and Segmenter should give identical results")
+		}
 	}
 }
