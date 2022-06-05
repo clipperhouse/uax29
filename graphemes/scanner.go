b3669d92bdcc22c8e873a65ea211f7cc82b1f626
Matt Sherman <mwsherman@gmail.com>
2022-06-04 22:18:37 -0400

    Docs

--- graphemes/scanner.go
+++ graphemes/scanner.go
@@ -7,7 +7,7 @@ import (
 	"github.com/clipperhouse/uax29/iterators"
 )
 
-// NewScanner Scanner, to tokenize graphemes per https://unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries.
+// NewScanner returns a Scanner, to tokenize graphemes per https://unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries.
 // Iterate through graphemes by calling Scan() until false, then check Err(). See also the bufio.Scanner docs.
 func NewScanner(r io.Reader) *iterators.Scanner {
 	scanner := iterators.NewScanner(r, SplitFunc)
