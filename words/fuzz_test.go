b1e5de065cf9b1d74dc3f309eeec19bbcb58e9be
Matt Sherman <mwsherman@gmail.com>
2022-06-15 22:45:54 -0400

    Start some fuzzing support

---
+++ words/fuzz_test.go
@@ -0,0 +1,59 @@
+package words_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+	"unicode/utf8"
+
+	"github.com/clipperhouse/uax29/words"
+)
+
+func FuzzWords(f *testing.F) {
+	file, err := ioutil.ReadFile("testdata/sample.txt")
+	if err != nil {
+		f.Error(err)
+	}
+
+	// add a large one
+	f.Add(file)
+
+	// add a bunch of small ones
+	lines := bytes.Split(file, []byte("\n"))
+	for _, line := range lines {
+		f.Add(line)
+	}
+
+	badUTF8, err := ioutil.ReadFile("testdata/UTF-8-test.txt")
+	if err != nil {
+		f.Error(err)
+	}
+	f.Add(badUTF8)
+
+	f.Fuzz(func(t *testing.T, original []byte) {
+		var segs [][]byte
+		valid1 := utf8.Valid(original)
+		seg := words.NewSegmenter(original)
+		for seg.Next() {
+			segs = append(segs, seg.Bytes())
+		}
+		if seg.Err() != nil {
+			t.Error(seg.Err())
+		}
+
+		var roundtrip []byte
+		for _, s := range segs {
+			roundtrip = append(roundtrip, s...)
+		}
+
+		if !bytes.Equal(roundtrip, original) {
+			t.Error("bytes did not roundtrip")
+		}
+
+		valid2 := utf8.Valid(roundtrip)
+
+		if valid1 != valid2 {
+			t.Error("utf8 validity of original did not match roundtrip")
+		}
+	})
+}
