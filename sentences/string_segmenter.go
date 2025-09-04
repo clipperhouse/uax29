2ac44737547ff24088aa8fdff475f5443c104f09
Matt Sherman <mwsherman@gmail.com>
2025-09-04 18:50:45 -0400

    Expose StringSegmenter on sentences, words

---
+++ sentences/string_segmenter.go
@@ -0,0 +1,28 @@
+package sentences
+
+import "github.com/clipperhouse/uax29/iterators"
+
+// NewSegmenter retuns a Segmenter, which is an iterator over the source text.
+// Iterate while Next() is true, and access the segmented graphemes via Bytes().
+func NewStringSegmenter(data string) *iterators.StringSegmenter {
+	seg := iterators.NewStringSegmenter(SplitFunc)
+	seg.SetText(data)
+	return seg
+}
+
+// SegmentAll will iterate through all tokens and collect them into a [][]byte.
+// This is a convenience method -- if you will be allocating such a slice anyway,
+// this will save you some code. The downside is that this allocation is
+// unbounded -- O(n) on the number of tokens. Use Segmenter for more bounded
+// memory usage.
+func StringSegmentAll(data string) []string {
+	// Optimization: guesstimate that the average grapheme is 1 bytes,
+	// allocate a large enough array to avoid resizing
+	result := make([]string, 0, len(data))
+	seg := NewStringSegmenter(data)
+	for seg.Next() {
+		result = append(result, seg.Text())
+	}
+
+	return result
+}
